a05ce4ea40deaa0e1f4b970fedd0a041717d19a4
Gewo <gewitt.hiller@gmail.com>
2023-01-15 19:08:05 +0100

    init commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x64680A11540Ee82B533197BCFd4E41CF4b23FAB3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
